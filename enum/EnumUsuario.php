c88c236c73747243e80d4ed1934f96e405ce0138
danieltorres31 <danieltorres31@outlook.com>
2019-10-29 21:21:17 -0300

    feat: Ajusta enum usuario

--- enum/EnumUsuario.php
+++ enum/EnumUsuario.php
@@ -6,20 +6,21 @@
     define("UsuarioExcluido", "S");
     define("UsuarioNaoExcluido", "N");
     
-    define("ERRO_NOME_OBRIGATORIO", "O nome do usuário é de preenchimento obrigatório.");
-    define("ERRO_EMAIL_OBRIGATORIO", "O e-mail é um campo obrigatório");
-    define("ERRO_SENHA_OBRIGATORIA", "A senha é de preenchimento obrigatório");
-    define("ERRO_REPETIR_SENHA", "Você deve repetir a senha corretamente");
+    define("ERRO_NOME_OBRIGATORIO_USUARIO", "O nome do usuário é de preenchimento obrigatório.");
+    define("ERRO_EMAIL_OBRIGATORIO_USUARIO", "O e-mail é um campo obrigatório");
+    define("ERRO_SENHA_OBRIGATORIA_USUARIO", "A senha é de preenchimento obrigatório.");
+    define("ERRO_CONF_SENHA_OBRIGATORIA_USUARIO", "A confirmação de senha é de preenchimento obrigatório.");
+
+    define("ERRO_REPETIR_SENHA_USUARIO", "Você deve repetir a senha corretamente");
     define("ERRO_USUARIO_CRIADO", "O usuário não foi criado");
     define("SUCESSO_USUARIO_CRIADO", "Usuário criado com sucesso.");
-    define("ERRO_EMAIL_EXISTE", "O e-mail digitado já está associado a uma conta.");
-    define("ERRO_NENHUM_USUARIO", "Nenhum usuário foi encontrado.");
+    define("ERRO_EMAIL_EXISTE_USUARIO", "O e-mail digitado já está associado a uma conta.");
+    define("ERRO_NENHUM_USUARIO_USUARIO", "Nenhum usuário foi encontrado.");
     define("SUCESSO_USUARIO_EXCLUIDO", "O usuário foi excluído com sucesso.");
     define("ERRO_USUARIO_EXCLUIDO", "O usuário não foi excluído.");
     define("SUCESSO_ALTERAR_USUARIO", "Alterações realizadas com sucesso.");
     define("ERRO_ALTERAR_USUARIO", "Não foi possível alterar os dados do usuário.");
-    define("ERRO_SENHA_ANTIGA_INCORRETA", "Senha antiga inválida.");
-
+    define("ERRO_SENHA_ANTIGA_INCORRETA_USUARIO", "Senha antiga inválida.");
     
     ?>   
     
